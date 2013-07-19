b2ea7191104deabd46698a9de641ad9ba8312f8c
Trieulh <trieulh.bk@gmail.com>
2013-07-19 16:49:22 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoView::Application.config.secret_token = '2a277dfd477d059261e8f064793670d37af026f29ccd0b93700cb91d7aad517ec4d90ed7a8245f72b143452ff000436bc930ed1490f687d04f22abf99f1a68d3'
